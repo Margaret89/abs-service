c1b04090847ec214792ee7f9c5ded2be59e58ce7
DESKTOP-1SDMCHM\Margarita <match14@yandex.ru>
2019-10-19 22:33:12 +0300

    Часть страницы кузовного ремонта

--- public/js/main.js
+++ public/js/main.js
@@ -142,4 +142,48 @@ $(document).ready(function () {
 			e.preventDefault();
 		});
 	});
+
+// Табуляция
+	if ($('.js-tabs-page').length) {
+		if (!$('.js-tabs-page-item').hasClass('active')) {
+			$('.js-tabs-page-list').each(function(){
+				$(this).find('.js-tabs-page-item:first').addClass("active");
+			});
+
+			$('.js-tabs-page-content').each(function(){
+				$(this).find('.js-tabs-page-content-item:first').addClass("active");
+				$(this).find('.js-tabs-page-content-item:first').fadeIn();
+			});
+		}else{
+			$('.js-tabs-page-content').find('#' + $('.js-tabs-page-item.active').attr('data-item')).addClass("active");
+			$('.js-tabs-page-content').find('#' + $('.js-tabs-page-item.active').attr('data-item')).fadeIn();
+		}
+
+		$('.js-tabs-page-item').click(function(e) {
+			e.preventDefault();
+			var $parent = $(this).parents('.js-tabs-page');
+
+			$parent.find('.js-tabs-page-content-item').hide();
+			$parent.find('.js-tabs-page-content-item').removeClass('active');
+			$parent.find('.js-tabs-page-item').removeClass('active');
+
+			$(this).addClass("active");
+			$parent.find('#' + $(this).attr('data-item')).addClass("active");
+			$parent.find('#' + $(this).attr('data-item')).fadeIn();
+		});
+	}
+
+// Подсветка элементов машины при наведении
+if ($('.js-elem-car').length) {
+	$('.js-elem-car').hover(
+		function(){
+			var idItem = $(this).attr('id');
+
+			$('.js-elem-drawing').find('#'+idItem).addClass('hover');
+			$('.js-tabs-page-content-item.active').find('#'+idItem).addClass('hover');
+		},
+		function(){
+			$('.js-elem-car').removeClass('hover');
+		});
+	}
 });
